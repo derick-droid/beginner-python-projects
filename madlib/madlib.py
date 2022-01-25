4b59818f7f2774841c77a6128f4623c275a4dd34
derick-droid <derrickokinda9@gmail.com>
2022-01-25 15:59:07 +0300

    madlib project

---
+++ madlib/madlib.py
@@ -0,0 +1,37 @@
+youtuber = "derrick okinda"
+print("subscribe to {}" .format(youtuber))
+print("subscribe to " + youtuber)
+print(f"subscribe to {youtuber}")
+
+"""
+larger madlib project
+"""
+first_noun = input("enter noun: ")
+second_noun = input("enter noun: ")
+adjective_1 = input("enter adjective: ")
+madlib = f"""
+Human activities generate three broad sources of {first_noun} pollution: stationary or point, mobile, and
+indoor. In developing countries especially in the {second_noun} area, indoor air pollution {adjective_1} using
+open fires for cooking and heating may be a serious problem. Industries, power plants etc. are
+the cause of stationary air pollution. But in urban areas – both developing and developed
+countries, it is predominantly mobile or vehicular pollution that contributes to overall air
+quality problem. In Delhi, the data shows that of the total 3,000 metric tonnes of pollutants1
+belched out everyday, close to two-third (66%) is from vehicles. Similarly, the contribution of
+vehicles to urban air pollution is 52% in Bombay and close to one-third in Calcutta.2
+ Katz
+(1994) has estimated that in Santiago, Chile, wherever pollution concentration exceeds ambient
+standards, mobile sources or vehicles are the cause. Similarly, in case of Budapest, Hungary,
+transport is the dominant source of emissions except sulphur dioxide (SO2), contributing 57%
+of Oxides of Nitrogen (NOx), 80% of lead (Pb), 81% of carbon monoxide (CO) and 75% of
+hydrocarbon (HC) emissions (Lehoczki, 2000).
+
+A number of countries have targeted vehicles and associated sectors (such as, fuel) to
+curb the menace. Notable successful initiatives are: conversion of public transport from diesel
+to CNG in Delhi, switching of Vikrams (tuk-tuks) from diesel to electricity in Kathmandu
+valley, shifting from leaded to unleaded gasoline in many countries etc. Still the pollution
+problem in urban cities may continue to loom large due to ever-burgeoning vehicular
+population, which is outpacing any such measure and road network development. Following
+data gives a glimpse of such skewed growth. Against 1.9 million vehicular population in 19
+
+"""
+print(madlib)
